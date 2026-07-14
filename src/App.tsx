da84d748d94b78305f900b894684609bafee7aa6
Paul Pop <paulpop122@gmail.com>
2026-07-14 10:40:14 +0300

    feat: update Vercel Analytics import to use the correct package for React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { HelmetProvider } from 'react-helmet-async';
 
 import Navigator from 'Navigator';
